7ef5848b8623d4294c04779f8dfc94411c32b533
M. Mücahit TEZCAN <mmucahit.tezcan@gmail.com>
2020-10-17 22:07:05 +0300

    Teleport is awesome 🐱‍👤

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1>{{ msg }}</h1>
+  <p>{{ body.text }}</p>
+  <div id="cont1"></div>
+  <div id="cont2"></div>
+  <button @click.prevent="hereOrThere = '1'">cont1</button>
+  <button @click.prevent="hereOrThere = '2'">cont2</button>
+  <teleport :to="`#cont${hereOrThere}`">
+    <div class="telecont">
+      <p>I am the same node. I am just being moved around</p>
+    </div>
+  </teleport>
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { ref, reactive } from 'vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
+export default {
+  setup() {
+    const msg = ref('Vue3 Teleport Example');
+    const teleported = ref(false);
+    const hereOrThere = ref('1');
+    const body = reactive({
+      text: 'Click to teleport the node to the other container',
+    });
+    return {
+      msg,
+      teleported,
+      hereOrThere,
+      body,
+    };
   },
-});
+
+};
 </script>
 
 <style>
+* {
+  margin: 10px;
+  box-sizing: border-box;
+}
+
+button {
+  border: none;
+  background: palegreen;
+  padding: 20px;
+  cursor: pointer;
+}
+
+.telecont p {
+  color: aliceblue;
+}
+
+#cont1 {
+  position: absolute;
+  bottom: 20px;
+  left: 0;
+  height: 40vh;
+  width: 50vw;
+  background: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#cont2 {
+  position: absolute;
+  bottom: 20px;
+  right: 0;
+  height: 40vh;
+  width: 50vw;
+  background: orange;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
